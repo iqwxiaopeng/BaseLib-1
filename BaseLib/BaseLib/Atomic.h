064594253f313e89ce815c33eede18315f05f539
wangqipeng <wangqipeng2008@gmail.com>
2012-09-15 22:47:29 +0800

    Update BaseLib/BaseLib/Atomic.h

--- BaseLib/BaseLib/Atomic.h
+++ BaseLib/BaseLib/Atomic.h
@@ -1,28 +1,29 @@
 #ifndef ATOMIC_H_
 #define ATOMIC_H_
 #include <stdint.h>
+namespace BaseLib 
+{
 template <typename T>
 class Atomic:boost::noncopyable 
 {
 public:
-    Atomic()
-    :valueM(0)
+    Atomic():value_(0)
     {
-
     }
 
     T fetch()
     {
-        return __sync_val_compare_and_swap(&valueM,0,0); 
+        return __sync_val_compare_and_swap(&value_,0,0); 
     }
 
     T fetchAndAdd(T x)
     {
-        return __sync_fetch_and_add(&valueM,x);  
+        return __sync_fetch_and_add(&value_, x);  
     }
 private:
-    volatile T valueM;
+    volatile T value_;
 };
 
 typedef Atomic<int> atomic_int;
+}
 #endif
